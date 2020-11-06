ddca7b577c7bcd951f3f728ce15eb5bcc93a8020
William <williampereira21@gmail.com>
2020-11-06 12:42:30 -0300

    configurações do padrão de cod

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router }  from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res)=> {
-    return res.json({ok: true});
-})
+routes.get('/teste', (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
