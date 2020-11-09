360b8b91c934852ed18cf6b89d064ce1aff62a59
William <williampereira21@gmail.com>
2020-11-09 17:40:17 -0300

    config sequelize não finalizada

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,15 @@
 module.exports = {
   env: {
-    es6: true,
+    es2021: true,
     node: true,
   },
   extends: [
     'airbnb-base',
-    'prettier'
   ],
-  plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-underscore-dangle": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" } ]
   },
 };
-
