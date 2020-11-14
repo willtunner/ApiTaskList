097e7b26d1b577b45ed3a8e939c4596cc3f2bbeb
WillTunner <williampereira21@gmail.com>
2020-11-14 13:18:45 -0300

    yup para validações configurado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,19 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    // Define um schema para o Yup
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação!' });
+    }
+
     // Verifica se existe algum email já cadastrado
     const userExist = await User.findOne({
       where: { email: req.body.email },
